e326e4d0e6a4f2c9d8deb0e964ee66119104b674
noki03 <noki03@users.noreply.github.com>
2026-04-19 01:19:28 +0800

    fix01

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Employee;
 use App\Models\Store;
 use Illuminate\Http\Request;
-use Illuminate\Database\QueryException;
-
 
 class EmployeeController extends Controller
 {   
